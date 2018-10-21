bec45d021f83a4d52fe2147aa94985002d8f4671
Gregary Pergrossi <gpergrossi@gmail.com>
2018-10-21 15:09:28 -0500

    first commit

---
+++ src/main/java/com/gpergrossi/procedural_generation/App.java
@@ -0,0 +1,11 @@
+package com.gpergrossi.procedural_generation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
